d3a25ac7d2bc447f10ffa627942b1ba7457b807c
Brempong Appiah Dankwah <brempong.dankwah@amalitech.com>
2025-04-08 15:13:21 +0000

    Add explicit schemas for rental, location, user, and vehicle data in location_vehicle.py and user_transaction.py

--- location_vehicle.py
+++ location_vehicle.py
@@ -1,20 +1,74 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
 spark = SparkSession.builder.appName("UserTransaction").getOrCreate()
 
+# Define schemas
+rental_schema = StructType([
+    StructField("rental_id", StringType(), True),
+    StructField("user_id", StringType(), True),
+    StructField("vehicle_id", StringType(), True),
+    StructField("rental_start_time", TimestampType(), True),
+    StructField("rental_end_time", TimestampType(), True),
+    StructField("pickup_location", IntegerType(), True),
+    StructField("dropoff_location", IntegerType(), True),
+    StructField("total_amount", DoubleType(), True)
+])
+
+location_schema = StructType([
+    StructField("location_id", IntegerType(), True),
+    StructField("location_name", StringType(), True),
+    StructField("address", StringType(), True),
+    StructField("city", StringType(), True),
+    StructField("state", StringType(), True),
+    StructField("zip_code", IntegerType(), True),
+    StructField("latitude", DoubleType(), True),
+    StructField("longitude", DoubleType(), True)
+])
+
+user_schema = StructType([
+    StructField("user_id", StringType(), True),
+    StructField("first_name", StringType(), True),
+    StructField("last_name", StringType(), True),
+    StructField("email", StringType(), True),
+    StructField("phone_number", StringType(), True),
+    StructField("driver_license_number", StringType(), True),
+    StructField("driver_license_expiry", DateType(), True),
+    StructField("creation_date", DateType(), True),
+    StructField("is_active", IntegerType(), True)
+])
+
+vehicle_schema = StructType([
+    StructField("active", IntegerType(), True),
+    StructField("vehicle_license_number", StringType(), True),
+    StructField("registration_name", StringType(), True),
+    StructField("license_type", StringType(), True),
+    StructField("expiration_date", StringType(), True),
+    StructField("permit_license_number", StringType(), True),
+    StructField("certification_date", DateType(), True),
+    StructField("vehicle_year", IntegerType(), True),
+    StructField("base_telephone_number", StringType(), True),
+    StructField("base_address", StringType(), True),
+    StructField("vehicle_id", StringType(), True),
+    StructField("last_update_timestamp", StringType(), True),
+    StructField("brand", StringType(), True),
+    StructField("vehicle_type", StringType(), True)
+])
+
+# File URLs
 location_url = "s3://vehicle-rental-marketplace/input-data/locations.csv"
 user_url = "s3://vehicle-rental-marketplace/input-data/users.csv"
 transaction_url = "s3://vehicle-rental-marketplace/input-data/rental_transactions.csv"
 vehicle_url = "s3://vehicle-rental-marketplace/input-data/vehicles.csv"
+output_path = "s3://vehicle-rental-marketplace/output-data/user_transactions"
 
-output_path = "s3://vehicle-rental-marketplace/output-data/location-vehicle"
+# Read CSV files with explicit schemas
+locations = spark.read.csv(location_url, header=True, schema=location_schema)
+users = spark.read.csv(user_url, header=True, schema=user_schema)
+rental = spark.read.csv(transaction_url, header=True, schema=rental_schema)
+vehicles = spark.read.csv(vehicle_url, header=True, schema=vehicle_schema)
 
-# Read the CSV files from S3
-locations = spark.read.csv(location_url, header=True, inferSchema=True)
-users = spark.read.csv(user_url, header=True, inferSchema=True)
-rental = spark.read.csv(transaction_url, header=True, inferSchema=True)
-vehicles = spark.read.csv(vehicle_url, header=True, inferSchema=True)
 
 # JOin the rental transactions with vehicle data
 rental_vehicle_df = rental.join(vehicles, "vehicle_id", "left")
